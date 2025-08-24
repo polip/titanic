1136bfd01e20792813cd2b9fe391e9f432d2d605
polip <lozo.ivan@gmail.com>
2025-08-24 12:17:20 +0200

    renv

--- write manifest.R
+++ write manifest.R
@@ -1,7 +1,10 @@
 # write manifest using rsconnect
 # Load rsconnect package
 library(rsconnect)
-
-
+install.packages("ranger")
+ranger::fit(   )
 # Write manifest for the current directory
 writeManifest(appDir = ".", appFiles = "app.R", appPrimaryDoc = NULL)
+renv::snapshot()
+
+library(ranger)
