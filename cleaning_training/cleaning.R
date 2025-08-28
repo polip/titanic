3174f236af1d6147bf3b0c694288938e7bd454a3
polip <lozo.ivan@gmail.com>
2025-08-28 11:18:48 +0200

    clean

---
+++ cleaning_training/cleaning.R
@@ -0,0 +1,18 @@
+### Cleaning and change of data types
+
+library(tidyverse)
+train_clean <- train %>% 
+  janitor::clean_names() %>% 
+  mutate(survived=factor(survived), survived=fct_relevel(survived,"0",after=1))  %>% 
+  mutate(pclass=factor(pclass,labels = c("1st","2nd","3rd"))) %>% 
+  mutate(sex = factor(sex)) |> 
+  mutate(embarked= factor(embarked)) |> 
+  mutate(title=str_extract(name,"\\,\\s*(.*?)\\s*\\."), title=str_remove(title,", "), title=str_remove(title,"\\."), title=factor(title)) 
+
+
+
+
+train_clean <- train_clean |> 
+  select(-name, -ticket,-cabin)
+
+train_clean |> write_rds("data/train_clean.rds")
--- cleaning_training/titanic_training.R
+++ cleaning_training/titanic_training.R
@@ -3,27 +3,13 @@ library(readr)
 library(forcats)
 library(stringr)
 
-train <- read_csv("data/train.csv") |> 
-  janitor::clean_names()
+train_clean <- read_rds("data/train_clean.rds") 
+train_clean |> glimpse()
 
-train <- train %>% 
-  janitor::clean_names() %>% 
-  mutate(survived=factor(survived), survived=fct_relevel(survived,"0",after=1))  %>% 
-  mutate(pclass=factor(pclass,labels = c("1st","2nd","3rd"))) %>% 
-  mutate(sex = factor(sex)) |> 
-  mutate(embarked= factor(embarked)) |> 
-  mutate(title=str_extract(name,"\\,\\s*(.*?)\\s*\\."), title=str_remove(title,", "), title=str_remove(title,"\\."), title=factor(title)) 
-
-train |> glimpse()
-
-train <- train |> 
-  select(-name, -ticket,-cabin)
-
-train |> write_rds("data/train_final.rds")
 
 
 ### recipe
-recipe <- recipe(survived~., train) %>% 
+recipe <- recipe(survived~., train_clean) %>% 
   update_role(passenger_id, new_role = "id variable") %>% 
   step_impute_knn(c(age,fare)) %>% 
   step_novel(all_nominal_predictors()) %>% 
@@ -65,24 +51,26 @@ rf_wf <- workflow() |>
 
 #conf_mat_resampled(fit_resamples)
 
-train_fit <- fit(rf_wf, train)
+train_fit <- fit(rf_wf, train_clean)
 
 train_fit |> write_rds(
-  "titanic_model.rds")
+  "/home/ivan/Documents/titanic/titanic_model.rds")
+
 
 library(vip)
-vip(train_fit)
+vip (train_fit)
 
 
 #### test data
 
-test <- read_csv("data/test.csv") %>% 
-  janitor::clean_names() %>% 
-  #mutate(pclass=factor(pclass,labels = c("1st","2nd","3rd"))) %>% 
-  mutate(title=str_extract(name,"\\,\\s*(.*?)\\s*\\."), title=str_remove(title,", "), title=str_remove(title,"\\.")) %>% 
+test <- read_csv("data/test.csv") |> 
+  janitor::clean_names() |> 
+  #mutate(pclass=factor(pclass,labels = c("1st","2nd","3rd"))) |> 
+  mutate(title=str_extract(name,"\\,\\s*(.*?)\\s*\\."), title=str_remove(title,", "), title=str_remove(title,"\\.")) |> 
   mutate(across(where(is.character),factor))  
 
-test %>% count(title)
+test |> 
+ count(title)
 
 test_pred <- predict(train_fit,test,type = "prob")
 test_pred_class <- predict(train_fit,test,type = "class")
@@ -94,6 +82,6 @@ sample_submission <- bind_cols(
   test %>% select(passenger_id),test_pred_class %>% select(.pred_class) 
 )
 
-sample_submission <- sample_submission %>% 
+sample_submission <- sample_submission |> 
   rename("PassengerID"=passenger_id,"Survived"=.pred_class)
 sample_submission %>% write_csv("data/sample_submission_v2.csv")
